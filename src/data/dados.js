bdeea4fe6feff14bcdaa1a1b1b39398f3396f8f2
Ana <maganha2528@gmail.com>
2024-06-13 13:41:47 -0300

    adiciona a listagem de precos

---
+++ src/data/dados.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id:"1",
+        nome:"Notebook Dell Inspiron X",
+        preco:"4399,00",
+        fabricante:"Dell",
+        cor:"Preta",
+        linkImagem:"https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg"
+    },
+    {
+        id:"2",
+        nome:"Sorvete de Pistache",
+        preco:"150,00",
+        fabricante:"Bacio di Latte",
+        cor:"Pistache",
+        linkImagem:"https://cdn.pixabay.com/photo/2021/01/18/12/49/ice-cream-5928048_1280.jpg"
+    },
+    {
+        id:"3",
+        nome:"Fini Diversos",
+        preco:"20,99",
+        fabricante:"Finni",
+        cor:"COlorido",
+        linkImagem:"https://cdn.pixabay.com/photo/2013/08/10/18/13/candies-171342_1280.jpg"
+    }
+]

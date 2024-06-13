7f6ebd230e98ec90b03f5c81be971a5e6dda7d05
Ana <maganha2528@gmail.com>
2024-06-13 09:19:57 -0300

    adiciona o paper ao projeto

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,4 +1,7 @@
-import {View, Text, Button} from "react-native"
+import { View } from "react-native"
+import { Card, Text, Button, Avatar } from "react-native-paper";
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 export default function HomeScreen(props) {
     return (
@@ -16,6 +19,20 @@ export default function HomeScreen(props) {
             <Button
                 title='Ir para Contato'
                 onPress={() => props.navigation.navigate('Contato')} />
+
+            <Card>
+                <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+                <Card.Content>
+                    <Text variant="titleLarge">Card title</Text>
+                    <Text variant="bodyMedium">Card content</Text>
+                </Card.Content>
+                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                <Card.Actions>
+                    <Button>Cancel</Button>
+                    <Button>Ok</Button>
+                </Card.Actions>
+            </Card>
+
         </View>
     );
 }
